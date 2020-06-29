303c9808603d32830a7d26e057511d30196ca1d6
Priya Roy <17bce103@nirmauni.ac.in>
2020-06-29 20:23:33 +0530

    Updates

---
+++ precache-manifest.29c345414c693fd9a538e00118db0881.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4000f603be471b2f69e064264cb2a2e9",
+    "url": "/finalfrontend/index.html"
+  },
+  {
+    "revision": "2f43ebf3e3ef94902387",
+    "url": "/finalfrontend/static/css/2.3b7463e5.chunk.css"
+  },
+  {
+    "revision": "ea9f2c2a0af733c7e0ba",
+    "url": "/finalfrontend/static/css/main.290eccc3.chunk.css"
+  },
+  {
+    "revision": "2f43ebf3e3ef94902387",
+    "url": "/finalfrontend/static/js/2.71ed4d95.chunk.js"
+  },
+  {
+    "revision": "ce0a40b6bf1d85155b27d215da2bca79",
+    "url": "/finalfrontend/static/js/2.71ed4d95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9f2c2a0af733c7e0ba",
+    "url": "/finalfrontend/static/js/main.a8f65550.chunk.js"
+  },
+  {
+    "revision": "8a1d1eb56e61d7baeeb4",
+    "url": "/finalfrontend/static/js/runtime-main.37f9cb6c.js"
+  }
+]);
